41bab2850ddbac2d943d8c8b3caac3f74dbf73bd
marcos0801 <angulomarcos97@gmail.com>
2019-11-20 23:37:18 -0500

    Agregar archivos de proyecto.

---
+++ ClassLibrary1aPPPROGRAMACIONCAPAS/FACTORIAL.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1aPPPROGRAMACIONCAPAS
+{
+    public static class FACTORIAL
+    {
+        /// <summary>
+        /// calcula el factorial de un número
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public  static long Fact(int num)
+        {
+            long f = 1;
+            if (num == 0 || num == 1)
+                return f;
+
+            for (int i = 1; i <= num; i++)
+                f = f * i;
+
+            return f;
+        }
+        /// <summary>
+        /// calcula el cuadrado de un número
+        /// </summary>
+        /// <param name="num"></param>
+        /// <returns></returns>
+        public static double  cuadrado(double num)
+        {
+            return (num * num);
+
+        }
+
+    }
+}
