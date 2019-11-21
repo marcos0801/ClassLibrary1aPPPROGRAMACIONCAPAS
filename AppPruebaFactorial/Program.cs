356e7c1d1298eb2beb6319223de0312329c929b3
marcos0801 <angulomarcos97@gmail.com>
2019-11-21 00:19:19 -0500

    factorialde 5

---
+++ AppPruebaFactorial/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPruebaFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 5;
+            double res = ClassLibrary1aPPPROGRAMACIONCAPAS.FACTORIAL.Fact(num);
+            Console.WriteLine("El factorial es : {0} es : {1}",num,res);
+            Console.ReadKey(); //pausa
+        }
+    }
+}
